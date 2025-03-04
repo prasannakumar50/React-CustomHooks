770da54137b273979855a40ae13258db1f564bee
Prasanna Kumar <63695489+prasannakumar50@users.noreply.github.com>
2025-03-04 13:38:22 +0000

    chore: updated useStorage Hook

--- src/customHooks/useLocalStorage.jsx
+++ src/customHooks/useLocalStorage.jsx
@@ -1,1 +1,19 @@
-export function useLocalStorage() {}
+import { useEffect, useState } from "react";
+
+export function useLocalStorage(item, inputValue = null) {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem("value") || inputValue;
+    console.log("Stored Value..", storedValue);
+    return storedValue;
+  });
+
+  useEffect(() => {
+    if (value !== null) {
+      localStorage.setItem(item, value);
+      console.log("Updated Local Storage:", value);
+    }
+  }, [item, value]);
+
+  return { value, setValue };
+}
+
